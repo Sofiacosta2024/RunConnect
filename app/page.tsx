8edf8f1526c6f9b6eb5edff62f6279b67673f81d
Brian Crowley <crowley.brian2004@gmail.com>
2026-06-13 10:53:17 -0300

    Mejoro vista muro

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,15 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import StatsStrip from "./components/StatsStrip";
-import Feed from "./components/Feed/Feed";
-import { getSugerencias } from "@/app/sugerencias/actions";
+
 
 export default async function Page() {
-  const sugerenciasIniciales = await getSugerencias({
-    nivel: "intermedio",
-    distanciaMaxKm: 10,
-  }).catch(() => []);
 
   return (
     <div className="rc-root">
       <Navbar />
       <Hero />
       <StatsStrip />
-
-      <div className="rc-layout">
-        <Feed sugerenciasIniciales={sugerenciasIniciales} />
-      </div>
     </div>
   );
 }
